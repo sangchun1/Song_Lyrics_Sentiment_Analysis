11eb0735b5c9fbee087329f855c6616ce0ea7477
sangchun1 <sangchun241@gmail.com>
2025-05-01 19:30:58 +0900

    edit 05/01/2025

--- module/category/artist_analysis.py
+++ module/category/artist_analysis.py
@@ -13,10 +13,37 @@ def get_artist_emotion_ratio(df: pd.DataFrame, top_n=20) -> pd.DataFrame:
     return count.div(count.sum(axis=1), axis=0)
 
 # 아티스트별 Score
-def get_artist_emotion_score(df: pd.DataFrame, top_n=20) -> pd.DataFrame:
-    top_artists = df['artist'].value_counts().head(top_n).index.tolist()
-    df_artist = df[df['artist'].isin(top_artists)]
-    return df_artist.groupby('artist')[['emotion_score', 'normalized_emotion_score']].mean()
+def get_artist_emotion_score(
+    df: pd.DataFrame,
+    score_type: str = 'normalized_emotion_score',
+    top_n: int = 20,
+    ascending: bool = False
+) -> pd.DataFrame:
+    """
+    감정 점수 기준 상위 or 하위 N명 아티스트 추출
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        전체 데이터
+    score_type : str
+        기준 점수 ('emotion_score' 또는 'normalized_emotion_score')
+    top_n : int
+        추출할 아티스트 수
+    ascending : bool
+        True면 하위 N명, False면 상위 N명 추출
+
+    Returns
+    -------
+    pd.DataFrame
+        선택된 아티스트들의 평균 감정 점수
+    """
+    assert score_type in ['emotion_score', 'normalized_emotion_score'], "Invalid score_type"
+    
+    score_df = df.groupby('artist')[[score_type]].mean()
+    selected_df = score_df.sort_values(by=score_type, ascending=ascending).head(top_n)
+    
+    return selected_df
 
 # 아티스트별 Top-N Words
 def get_artist_top_words(df: pd.DataFrame, top_n=10, artist_n=20) -> pd.Series:
